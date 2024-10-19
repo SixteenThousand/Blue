966f29e3261bde08a2b17ad5b7467f3d46a53d27
Thomas Lindop <thomas16256@gmail.com>
2024-10-19 14:47:41 +0100

    >> revamp new-app script

--- scripts/new-app.sh
+++ scripts/new-app.sh
@@ -1,42 +1,58 @@
-for arg in $@
-do
-	if [ "$arg" = '-h' -o "$arg" = '--help' ]
-	then
-		cat <<- EOF
-		This script should be run as:
-		    bash ./scripts/new-app.sh APPNAME REPO_VISIBILITY
-		from the project top-level, i.e. Blue.
-		
-		APPNAME is the name of the application you want to add a 
-		configuration for, e.g. riverwm. The new config repo will be in a 
-		directory called ./configs/APPNAME, and its corresponding GitHub
-		repo will be called blue-APPNAME.
-		
-		REPO_VISIBILITY should be one of 'public' and 'private'. It controls 
-		the visibility of the GitHub repo. Defaults to 'private'.
-		EOF
-		exit 0
-	fi
-done
-
-if [ ! "$(basename $(pwd))" = 'Blue' ]
-then
-	echo -e "\e[1;31mYou can only run this script from the project \
-toplevel, i.e. Blue!\e[0m"
+function error {
+	echo -e "\e[1;31m$1\e[0m"
 	exit 1
-fi
+}
 
-name=$1
-visibility='--'${2:-'private'}
+function main {
+	case $1 in
+		-h|--help) print_help;;
+		github) github $2 $3;;
+		create-submodule) create_submodule $2;;
+		*) error 'Invalid option! Try --help instead';;
+	esac
 
-DESCRIPTION='Please see https://github.com/SixteenThousand/Blue for more information about this repository'
+	if [ ! "$(basename $(pwd))" = 'Blue' ]
+	then
+		error 'You can only run this from the project toplevel, i.e. Blue!'
+	fi
+}
+
+function github {
+	name=$1
+	visibility='--'${2:-'private'}
+	DESCRIPTION='Please see https://github.com/SixteenThousand/Blue for more information about this repository'
+	cd ./configs
+	gh repo create "blue-$name" --description "$DESCRIPTION" $visibility --clone
+	mv "blue-$name" $name
+}
+
+function create_submodule {
+	name=$1
+	git submodule add "https://github.com/SixteenThousand/blue-$name" "./configs/$name"
+}
+
+function print_help {
+	cat <<- EOF
+	This script handles the fiddly bits needed to start a new config in 
+	this project. It should be run from the project top-level, i.e. Blue.
+
+	USAGE:
+		[1mbash ./scripts/new-app.sh github APPNAME REPO_VISIBILITY[0m
+	Creates a github repo for the app, and clones the (empty) repo into 
+	this repo.
+	APPNAME is the name of the application you want to add a 
+	configuration for, e.g. riverwm. The new config repo will be in a 
+	directory called ./configs/APPNAME, and its corresponding GitHub
+	repo will be called blue-APPNAME.
+	REPO_VISIBILITY should be one of 'public' and 'private'. It controls 
+	the visibility of the GitHub repo. Defaults to 'private'.
+
+		[1mbash ./scripts/new-app.sh create-submodule APPNAME[0m
+	Adds a submodule for the repo at ./configs/APPNAME. Changes still 
+	have to be committed after running this.
+	EOF
+	exit 0
+}
 
 
-cd ./configs
-
-gh repo create "blue-$name" --description "$DESCRIPTION" $visibility --clone
-
-mv "blue-$name" $name
-
-cd ..
-git submodule add "https://github.com/SixteenThousand/blue-$name" "./configs/$name"
+main $@
