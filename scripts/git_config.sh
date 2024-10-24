704c6ab72b45ebabd7b0522e7c5cd7871a1b7291
Thomas <thomas16256@gmail.com>
2024-10-24 01:06:19 +0100

    >> make changes for debian system > make bash more resilient to uninstalled apps > remove windows newlines from git config script

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -1,25 +1,47 @@
 read -p 'What is your name?: ' NAME
 read -p 'What is email that you use with git?: ' EMAIL
-git config --global user.email={$EMAIL}
-git config --global user.name={$NAME}
+git config --global user.email $EMAIL
+git config --global user.name $NAME
 echo 'Personal details added!'
 
 git config --global init.defaultBranch main
+if which nvim > /dev/null
+then
+	git config --global core.editor nvim
+fi
+if which vim > /dev/null
+then
+	git config --global core.editor vim
+fi
+echo 'Options set!'
+
 git config --global alias.goto checkout
 git config --global alias.st status
 git config --global alias.diffs 'diff --cached'
 echo 'Aliases added!'
 
-if which dnf
+if which dnf > /dev/null
 then
 	sudo dnf install git-credential-oauth
-elif which go
+elif which go > /dev/null
 then
 	go install github.com/hickfordgit-credential-oauth@latest
 fi
 if git credential-oauth configure
+then
 	echo 'git-crendential-oauth installed & configured!'
+else
+	cat <<- EOF
+	git-credential-oauth failed to install.
+	Please do one of the following:
+	1) Install go and try this again.
+	2) Install manually some other way.
+	3) Use a system with DNF.
+	4) Give up.
+	
+	EOF
 fi
+
 
 cat <<- EOF
 You should now use 'gpg --gen-keys' (or something like that) to generate a 
