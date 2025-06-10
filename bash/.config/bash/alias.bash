7d3d37b341e2287f8bab69836d21b9a83f53ff4c
Thomas Lindop <thomas16256@gmail.com>
2025-06-10 14:26:34 +0100

    [bash] >> ignore python virtualenv's in tree and grep

--- bash/.config/bash/alias.bash
+++ bash/.config/bash/alias.bash
@@ -1,15 +1,19 @@
-alias l='ls --classify=always --color=always --group-directories-first --almost-all'
+alias l='ls --classify=always --color=always --almost-all'
 alias e="$EDITOR"
-alias el="nvim --cmd 'let g:sixteen_lsp=v:true'"
 alias f=nnn
-alias t='tree -a -I .git -I node_modules'
+# The --gitignore option isn't used here as I don't want files in 
+# $GIT_DIR/info/exclude to be excluded
+alias t='tree -a -I .git -I node_modules -I .venv'
 alias tma='tmux attach || tmux'
 alias tmd='tmux detach'
 alias sbcl='rlwrap sbcl'
 alias shell='rlwrap xargs -L 1'
 alias zathura='zathura --fork'
-alias yg='grep -RnH --exclude-dir=.git --exclude-dir=node_modules \
-	-I --color=always'
+alias yg='grep -RnHI \
+    --exclude-dir=.git \
+    --exclude-dir=node_modules \
+    --exclude-dir=.venv \
+	--color=always'
 # Pipe data into xcopy to save it in the clipboard,
 # and run this to print the clipboard contents to stdout
 alias xcopy='xclip -selection clipboard'
