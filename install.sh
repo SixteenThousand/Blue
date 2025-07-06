2b6d88162e7bb4aa1cbe7887eeb4404892086c33
Thomas <thomas16256@gmail.com>
2025-07-06 12:48:28 +0100

    [Blue] >> >> remove bashisms from install script

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ toplevel=$PWD
 for app in $(find -mindepth 1 -maxdepth 1 -type d -not -name '.git*'); do
   cd "$app"
   if make "$operation"; then
-    echo "\e[1;33m[${app}] configured!\e[0m"
+    printf "\e[1;33m[${app}] configured!\e[0m\n"
   else
-    echo "\e[1;31m[${app}] configuration failed!\e[0m"
+    printf "\e[1;31m[${app}] configuration failed!\e[0m\n"
   fi
   cd "$toplevel"
 done
